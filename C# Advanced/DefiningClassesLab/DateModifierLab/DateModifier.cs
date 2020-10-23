a6506942e0ee0a4bd1a1d52392643734897603c1
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2020-10-23 22:33:37 +0300

    Push more homework

---
+++ C# Advanced/DefiningClassesLab/DateModifierLab/DateModifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        public int GetDaysDifference(string start, string end)
+        {
+            DateTime startD = DateTime.Parse(start);
+            DateTime endD = DateTime.Parse(end);
+            
+            
+
+            var totalDiference = (int)(endD - startD).TotalDays;
+
+
+            return totalDiference;
+
+
+        }
+
+
+    }
+}
---
+++ C# Advanced/DefiningClassesLab/DateModifierLab/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string start = Console.ReadLine();
+            string end = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier();
+            var result = dateModifier.GetDaysDifference(start, end);
+            Console.WriteLine(result);
+            
+        }
+    }
+}
