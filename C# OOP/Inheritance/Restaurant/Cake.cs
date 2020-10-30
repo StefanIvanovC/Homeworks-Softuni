6576ceeeeb45f94dfece91f1af8c04a6e59f186c
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2020-10-30 17:39:26 +0200

    Add last homework for Itheritance

---
+++ C# OOP/Inheritance/Restaurant/Cake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const decimal DefoultCakePrice = 5m;
+        private const double defaultCalories = 1000;
+        private const double DefaultGrams = 250;
+        public Cake(string name)
+            : base(name, DefoultCakePrice, DefaultGrams, defaultCalories)
+        {
+        }
+    }
+}
---
+++ C# OOP/Inheritance/Restaurant/Coffee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+
+        private const double CoffeeMilliliters = 50;
+        private const decimal CoffeePrice = 3.50m;
+
+        public double Caffeine { get; set; }
+
+        public Coffee(string name, decimal price, double milliliters, double caffeine)
+            : base(name, CoffeePrice, CoffeeMilliliters)
+        {
+            this.Caffeine = caffeine;
+        }
+    }
+}
