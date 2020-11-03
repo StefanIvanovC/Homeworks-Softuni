738ceb6517649c78e1499ad0193eec1e54ba6e41
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2020-11-03 23:29:40 +0200

    Add third Exercise

---
+++ C# OOP/Encapsulation/03.Shopping Spree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.Shopping_Spree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
