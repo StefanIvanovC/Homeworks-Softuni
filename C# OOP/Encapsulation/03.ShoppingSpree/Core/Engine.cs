20642ac82d28d09ff4ce60f7f138cbd8d1e5bb8f
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2020-11-04 12:17:04 +0200

    Write some functions on the Engine

--- C# OOP/Encapsulation/03.ShoppingSpree/Core/Engine.cs
+++ C# OOP/Encapsulation/03.ShoppingSpree/Core/Engine.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using _03.ShoppingSpree.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace _03.ShoppingSpree.Core
 {
-    class Engine
+    public class Engine
     {
+        private readonly iCollection<>
+        
+        public Engine()
+        {
+
+        }
+
+        public void Run()
+        {
+            string[] peopleArgs = Console.ReadLine()
+                .Split(";", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            foreach (var personFor in peopleArgs)
+            {
+                string[] personArg = personFor.Split("=", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string personForRead = personArg[0];
+                decimal personMoney = decimal.Parse(personArg[1]);
+
+                Person person = new Person(personForRead,personMoney);
+
+            }
+
+            
+        }
+
     }
 }
