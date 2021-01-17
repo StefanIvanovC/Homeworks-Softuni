c0f032d956186224dc3e1a2dad4b5523f0f5e9bd
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-17 20:04:29 +0200

    Add Homework Sln with project

---
+++ Programming Fundamentals/Data Types and Variables/DataTypesAndVariables/DataTypesAndVariables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long firstN = long.Parse(Console.ReadLine());
+            long secondN = long.Parse(Console.ReadLine());
+            long thirdN = long.Parse(Console.ReadLine());
+            long fourthN = long.Parse(Console.ReadLine());
+
+            long sum = firstN + secondN;
+            long devided = sum / thirdN;
+            long multiply = devided * fourthN;
+
+            Console.WriteLine(multiply);
+        }
+    }
+}
