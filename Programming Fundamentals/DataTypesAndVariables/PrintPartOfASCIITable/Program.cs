cc393139e473abb67baa61747dfdc2a8b88eb5b6
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-17 20:50:51 +0200

    Add homework exersice

---
+++ Programming Fundamentals/DataTypesAndVariables/PrintPartOfASCIITable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int f = int.Parse(Console.ReadLine());
+            string fin = "";
+
+            for (int i = n; i <= f; i++)
+            {
+                fin += (char)i + " ";
+            }
+
+            Console.WriteLine(fin);
+        }
+    }
+}
