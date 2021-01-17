42929a1ff1737af1db585bb52a374d9e7a0b8a39
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-18 00:18:12 +0200

    Add homework

---
+++ Programming Fundamentals/DataTypesAndVariables/BeerKegs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            double biggestKeg = 0;
+            string bestModel = "";
+            for (int i = 0; i < count; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volume = Math.PI * Math.Pow(radius, 2) * height;
+
+                if (biggestKeg < volume)
+                {
+
+                    biggestKeg = volume;
+                    bestModel = model;
+                }
+
+            }
+
+            Console.WriteLine(bestModel);
+        }
+    }
+}
