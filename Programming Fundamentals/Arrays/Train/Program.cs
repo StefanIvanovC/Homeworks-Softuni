599edc95748bc359ccc56b5b79d46a348717c5e2
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-18 00:35:22 +0200

    Add Homework Arrays

---
+++ Programming Fundamentals/Arrays/Train/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+            var wagonsNums = new int[input];
+            var sum = 0;
+            for (int i = 0; i < input; i++)
+            {
+                wagonsNums[i] = int.Parse(Console.ReadLine());
+                sum += wagonsNums[i];
+            }
+
+            foreach (var item in wagonsNums)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
