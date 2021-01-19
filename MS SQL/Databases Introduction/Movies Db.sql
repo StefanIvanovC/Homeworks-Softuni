609391a80678d78b20b6812eba0f2d6f63d67d2e
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-20 00:17:11 +0200

    Add University Homework Tables

--- MS SQL/Databases Introduction/Movies Db.sql
+++ MS SQL/Databases Introduction/Movies Db.sql
@@ -1,39 +1,61 @@
-CREATE DATABASE University
-
-USE University
-
-CREATE TABLE Subjects
+CREATE TABLE Directors 
 (
-	SubjectID INT PRIMARY KEY IDENTITY,
-	SubjectName VARCHAR(30) NOT NULL
+	Id INT PRIMARY KEY,
+	DirectorName VARCHAR(90) NOT NULL,
+	Notes VARCHAR(MAX)
 )
 
-CREATE TABLE Majors
+INSERT INTO Directors VALUES 
+(1, 'Stefii1', 'dasdasd1'),
+(2, 'Stefii2', 'dasdasd2'),
+(3, 'Stefii3', 'dasdasd3'),
+(4, 'Stefii4', 'dasdasd4'),
+(5, 'Stefii5', 'dasdasd5')
+
+CREATE TABLE Genres
 (
-	MajorID INT PRIMARY KEY IDENTITY,
-	[Name] VARCHAR(30) NOT NULL
+	Id INT PRIMARY KEY,
+	GenreName VARCHAR(90) NOT NULL,
+	Notes VARCHAR(MAX)
 )
 
-CREATE TABLE Students
+INSERT INTO Genres VALUES
+(1,'Tanq1', 'Tanq1'),
+(2,'Tanq2', 'Tanq2'),
+(3,'Tanq3', 'Tanq3'),
+(4,'Tanq4', 'Tanq4'),
+(5,'Tanq5', 'Tanq5')
+
+CREATE TABLE Categories
 (
-	StudentID INT PRIMARY KEY IDENTITY,
-	StudentNumber int,
-	StudentName VARCHAR(30),
-	MajorID INT FOREIGN KEY REFERENCES Majors(MajorID)
+	Id INT PRIMARY KEY,
+	CategoryName VARCHAR(90) NOT NULL,
+	Notes VARCHAR(MAX)
 )
 
-CREATE TABLE PAYMENTS
+INSERT INTO Categories VALUES
+(1,'HORROR1', 'HORROR1'),
+(2,'HORROR2', 'HORROR2'),
+(3,'HORROR3', 'HORROR3'),
+(4,'HORROR4', 'HORROR4'),
+(5,'HORROR5', 'HORROR5')
+
+CREATE TABLE Movies 
 (
-	PaymentID INT PRIMARY KEY IDENTITY,
-	PaymentDate DATETIME,
-	PaymentAmount DECIMAL (12, 2),
-	StudentID INT FOREIGN KEY REFERENCES Students(StudentID)
+	Id INT PRIMARY KEY,
+	Title VARCHAR(90),
+	DirectorId INT,
+	CopyrightYear DATETIME,
+	 [Length] INT,
+	 GenreId INT,
+	 CategoryId VARCHAR(30),
+	 Rating INT,
+	 Notes VARCHAR(MAX)
 )
 
-CREATE TABLE Agenda 
-(
-	StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
-    SubjectID INT FOREIGN KEY REFERENCES Subjects(SubjectID)
-
-	CONSTRAINT PK_Student_Order PRIMARY KEY (StudentID,SubjectID)
-)
+INSERT INTO Movies VALUES 
+(1, 'Harry Poter', 1, 12/01/2021, 10, 5, 'Ujas1', 2, 'skuchen1'),
+(2, 'Harry Poter3', 1, 15/01/2021, 11, 4, 'Ujasi2', 3, 'skuchen2'),
+(3, 'Harry Poter2', 1, 18/01/2021, 12, 3, 'Ujas3', 4, 'skuchen3'),
+(4, 'Harry Poter4', 1, 17/01/2021, 13, 2, 'Ujasi4', 5, 'skuchen4'),
+(5, 'Harry Poter5', 1, 16/01/2021, 14, 1, 'Ujasi5', 6, 'skuchen5')
