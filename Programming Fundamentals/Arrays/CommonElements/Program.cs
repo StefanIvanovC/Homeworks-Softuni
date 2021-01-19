ce440cc2d8a4d354ed1321a7d8c283b6a445c7c3
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-19 18:31:30 +0200

    Add homework

---
+++ Programming Fundamentals/Arrays/CommonElements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = Console.ReadLine().Split();
+            string[] arr2 = Console.ReadLine().Split();
+            string result = "";
+            foreach (var word in arr1)
+            {
+                foreach (var word2 in arr2)
+                {
+                    if (word == word2)
+                    {
+                        result += word + " ";
+                    }
+
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
