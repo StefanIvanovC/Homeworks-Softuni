24909cc47cc6d8e9bd2a0fc9bc432aed2e816862
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-19 19:18:46 +0200

    Add SQL Query and project C#

---
+++ Programming Fundamentals/Arrays/ArrayRotation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            int numbersOfRotation = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbersOfRotation; i++)
+            {
+                var temp = arr[0];
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    arr[j] = arr[j + 1];
+
+                }
+                arr[arr.Length - 1] = temp;
+            }
+
+
+
+
+            Console.WriteLine(string.Join(" ", arr));
+
+        }
+    }
+}
