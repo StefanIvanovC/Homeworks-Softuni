fc4717808dcd47ae94615d342e31e020214fb1f1
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-21 17:37:16 +0200

    Add more homework

---
+++ Programming Fundamentals/Arrays/MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01.Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputList = Console.ReadLine()
+                .Split(' ').Select(int.Parse).ToList();
+
+            int maxSequenceLength = 0;
+            int maxSequenceStartIndex = 0;
+            int maxSequenceEndIndex = -1;
+
+            for (int i = 0; i < inputList.Count - 1; i++)
+            {
+                int startIndex = i;
+                int length = 1;
+
+                while (i < inputList.Count - 1 && inputList[i] == inputList[i + 1])
+                {
+                    length++;
+                    i++;
+                }
+                if (length > maxSequenceLength)
+                {
+                    maxSequenceLength = length;
+                    maxSequenceStartIndex = startIndex;
+                    maxSequenceEndIndex = i;
+                }
+            }
+            for (int i = maxSequenceStartIndex; i <= maxSequenceEndIndex; i++)
+            {
+                Console.Write(inputList[i] + " ");
+            }
+        }
+    }
+}
