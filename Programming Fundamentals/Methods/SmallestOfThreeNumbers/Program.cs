15e60d4a3a9c92fcebeb3db478555c3157640e78
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-21 17:43:35 +0200

    Add Methods Sln

---
+++ Programming Fundamentals/Methods/SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace METHODSs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Smallest(first, second, third));
+        }
+
+        static int Smallest(int a, int b, int c)
+        {
+            if (a < b && a < c)
+            {
+                return a;
+            }
+            else if (b < a && b < c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }
+    }
+}
