36d00fdc32d86527c1c6e94fdbd51632e98414d6
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-21 17:35:55 +0200

    Add new  project Exersice

---
+++ Programming Fundamentals/Arrays/EqualSum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.EqualSums
+{
+    class EqualSums
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var founNum = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var leftSum = 0;
+                var rightSum = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += input[j];
+                }
+
+                for (int k = i + 1; k < input.Length; k++)
+                {
+                    rightSum += input[k];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(Array.IndexOf(input, input[i]));
+                    founNum = true;
+                }
+            }
+
+            if (founNum == false)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
