d7902c707bc1b7c74a4dcb38ef91ae9e993f4d72
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-21 17:46:24 +0200

    Add second Exersice in Methods

---
+++ Programming Fundamentals/Methods/VowelsCount/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputText = Console.ReadLine().ToLower();
+            char[] Inputchars = inputText.ToCharArray();
+            int counter = 0;
+
+            for (int i = 0; i < Inputchars.Length; i++)
+            {
+                if (inputText[i] == 'a' || inputText[i] == 'e' || inputText[i] == 'i' ||
+                    inputText[i] == 'o' || inputText[i] == 'u' || inputText[i] == 'y')
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
