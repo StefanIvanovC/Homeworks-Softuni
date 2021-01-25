92f30b364faa5893fad77f45bf0812c79ae7bf24
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-25 17:16:06 +0200

    Add metrix in Methods

---
+++ Programming Fundamentals/Methods/NxNMatrix/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3x3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Print3x3(n);
+        }
+
+        static void Print3x3(int n)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
