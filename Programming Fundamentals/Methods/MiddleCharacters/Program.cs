85a73ba3698c56bf162de7685ed95eb7f61b7393
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-25 17:13:50 +0200

    Add homework

---
+++ Programming Fundamentals/Methods/MiddleCharacters/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace middlePrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Print(text);
+        }
+
+        static void Print(string text)
+        {
+            var textLenght = text.Length;
+
+            if (textLenght % 2 != 0)
+            {
+                var chToPrint = textLenght / 2;
+
+                for (int i = 1; i <= textLenght + 1; i++)
+                {
+                    if (i == chToPrint)
+                    {
+                        Console.WriteLine(text[i]);
+                    }
+                }
+
+            }
+
+            else if (textLenght % 2 == 0)
+            {
+                var chToPrint = textLenght / 2;
+
+                for (int i = 1; i <= textLenght + 1; i++)
+                {
+                    if (i == chToPrint)
+                    {
+                        Console.WriteLine(text[i - 1] + "" + text[i]);
+                    }
+                }
+            }
+        }
+    }
+}
