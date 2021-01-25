53759ef33a9698db8c8ec9d04d46b0f7108a2abb
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-25 17:10:20 +0200

    Add homework

---
+++ Programming Fundamentals/Methods/AddAndSubtract/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace chacher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+
+            var fin = SubtractMethod(SumMethod(first, second), third);
+            Console.WriteLine(fin);
+        }
+
+        static int SubtractMethod(int a, int b)
+        {
+            var subtract = a - b;
+            return subtract;
+        }
+
+        static int SumMethod(int a, int b)
+        {
+            var sum = a + b;
+            return sum;
+        }
+    }
+}
