f91d5c0302012d08d536d4f9b266f20fa0ea90f3
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-25 17:21:55 +0200

    Add homework

---
+++ Programming Fundamentals/Methods/FactorialDivision/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+            var one = Factoriel(first);
+            var two = Factoriel(second);
+
+            var devided = one / two;
+
+            Console.WriteLine($"{devided:f2}");
+        }
+
+        static double Factoriel(int a)
+        {
+            double factoriel = 1;
+
+            for (int i = a; i > 0; i--)
+            {
+                factoriel *= i;
+
+            }
+
+            return factoriel;
+        }
+    }
+}
---
+++ Programming Fundamentals/Methods/PalindromeIntegers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace mirrorMumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strInput = Console.ReadLine();
+
+            while (strInput != "END")
+            {
+                var start = strInput[0];
+                char end = ' ';
+
+                for (int i = 0; i < strInput.Length; i++)
+                {
+                    end = strInput[i];
+                }
+
+                if (start == end)
+                {
+                    Console.WriteLine("true");
+                }
+
+                else
+                {
+                    Console.WriteLine("false");
+                }
+
+                strInput = Console.ReadLine();
+            }
+
+        }
+    }
+}
