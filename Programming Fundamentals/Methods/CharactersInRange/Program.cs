db3569d2c0daba44bce7925e933fa6ebbdf762e6
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-01-25 17:06:12 +0200

    Add Exercise

---
+++ Programming Fundamentals/Methods/CharactersInRange/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstInput = char.Parse(Console.ReadLine());
+            var secondInput = char.Parse(Console.ReadLine());
+
+            int start = Math.Min(firstInput, secondInput);
+            int end = Math.Max(firstInput, secondInput);
+
+            for (int i = start + 1; i < end; i++)
+            {
+                char symbol = (char)i;
+
+                Console.Write(symbol + " ");
+            }
+        }
+    }
+}
