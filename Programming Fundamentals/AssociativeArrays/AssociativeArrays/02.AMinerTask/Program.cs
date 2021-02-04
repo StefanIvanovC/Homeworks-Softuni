bbc42ea8fc95c9f60195624d1ee56a284428f05c
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-04 18:21:30 +0200

    Add homework

---
+++ Programming Fundamentals/AssociativeArrays/AssociativeArrays/02.AMinerTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, int>();
+            var stop = "";
+            while (stop != "stop")
+            {
+                var input = Console.ReadLine();
+                if (input == "stop")
+                {
+                    break;
+                }
+                var quantity = int.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(input))
+                {
+                    dict[input] = quantity;
+                }
+                else
+                {
+                    dict[input] += quantity;
+                }
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine(string.Join(" ", $"{item.Key} -> {item.Value}"));
+            }
+        }
+    }
+}
