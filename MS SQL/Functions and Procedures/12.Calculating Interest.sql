1eccf658ddc7c883edfa2b746813fc7f839214ce
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-04 19:00:13 +0200

    Add Query

---
+++ MS SQL/Functions and Procedures/12.Calculating Interest.sql
@@ -0,0 +1,8 @@
+CREATE PROC usp_CalculateFutureValueForAccount (@acountId INT, @interestRate FLOAT)
+AS 
+SELECT a.Id, ah.FirstName, ah.LastName, a.Balance, dbo.ufn_CalculateFutureValue(a.Balance, @interestRate , 5)
+	FROM AccountHolders AS ah
+	JOIN Accounts AS a ON a.AccountHolderId = ah.Id
+	WHERE a.ID = @acountId
+
+	EXEC usp_CalculateFutureValueForAccount 1, 0.1
