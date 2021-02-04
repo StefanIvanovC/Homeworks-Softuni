4210950083ed63770e39149a4752c3b208f22961
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-04 18:15:18 +0200

    Add homework

---
+++ Programming Fundamentals/AssociativeArrays/AssociativeArrays/01.CountCharsInAString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssociativeArreys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var dict = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var curr = input[i];
+                if (curr == ' ')
+                {
+                    continue;
+                }
+                if (!dict.ContainsKey(curr))
+                {
+                    dict[curr] = 0;
+                }
+
+                dict[curr]++;
+
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine(string.Join(" ", $"{item.Key} -> {item.Value}"));
+            }
+        }
+    }
+}
