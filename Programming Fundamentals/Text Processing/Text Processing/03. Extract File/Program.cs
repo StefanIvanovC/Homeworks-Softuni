d3a5fb241ede41bc95c131056808c9af2bb10f6d
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-06 23:18:38 +0200

    Add homework

---
+++ Programming Fundamentals/Text Processing/Text Processing/03. Extract File/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+
+            string[] allPaths = path.Split("\\", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            string pathAndEnd = allPaths[3];
+            string[] clearPath = pathAndEnd.Split(".", StringSplitOptions.RemoveEmptyEntries);
+            string first = clearPath[0];
+            string second = clearPath.Last();
+
+            Console.WriteLine($"File name: {first}");
+            Console.WriteLine($"File extension: {second}");
+        }
+    }
+}
