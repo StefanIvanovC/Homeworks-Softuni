6b7fb8ef7ddd1655126fe063c44f65633da7536b
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-06 23:05:34 +0200

    Add homework

---
+++ Programming Fundamentals/Objects and Classes/Objects and Classes/03. Articles 2.0/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Article_Second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<Article> list = new List<Article>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+
+                Article myArticle = new Article(tokens[0], tokens[1], tokens[2]);
+                list.Add(myArticle);
+
+
+            }
+
+            var criteria = Console.ReadLine();
+            List<Article> sortedArticles = new List<Article>();
+
+            if (criteria == "title")
+            {
+                sortedArticles = list.OrderBy(x => x.Title).ToList();
+            }
+
+            else if (criteria == "content")
+            {
+                sortedArticles = list.OrderBy(x => x.Content).ToList();
+            }
+
+            else
+            {
+                sortedArticles = list.OrderBy(x => x.Author).ToList();
+            }
+
+            foreach (var item in sortedArticles)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        class Article
+        {
+            public Article(string title, string content, string author)
+            {
+                this.Title = title;
+                this.Content = content;
+                this.Author = author;
+            }
+
+            public string Title { get; set; }
+
+            public string Content { get; set; }
+
+            public string Author { get; set; }
+
+            public override string ToString()
+            {
+                return $"{this.Title} - {this.Content}: {this.Author}";
+            }
+        }
+    }
+}
