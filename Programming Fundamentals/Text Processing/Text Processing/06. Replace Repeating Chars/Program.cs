6a6aaa8a084121f3a7ee6a135d89657df48318ef
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-07 23:43:54 +0200

    Add homework

---
+++ Programming Fundamentals/Text Processing/Text Processing/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ReplaceRepeatingCharInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var countOfRemoves = 0;
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                var currChar = input[i];
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    var secondChar = input[j];
+                    if (currChar == secondChar)
+                    {
+                        countOfRemoves++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+                input = input.Remove(i + 1, countOfRemoves);
+                countOfRemoves = 0;
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
