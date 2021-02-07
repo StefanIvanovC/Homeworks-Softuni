116176fd8f8e26df19b5b97e2829da43649745ce
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-07 23:43:03 +0200

    Add homework

---
+++ Programming Fundamentals/Text Processing/Text Processing/04. Caesar Cipher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EncryptedText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] text = Console.ReadLine().ToCharArray();
+            string textForPrint = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currchar = text[i];
+                currchar += (char)3;
+                textForPrint += currchar;
+
+            }
+
+            Console.WriteLine(textForPrint);
+        }
+    }
+}
