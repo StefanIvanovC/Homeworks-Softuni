4193f11fd3395efa09b271f59adeb622df2f2790
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-07 23:50:29 +0200

    Add homework

---
+++ Programming Fundamentals/Stacks and Queues/Stacks and Queues/1. Reverse Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> charact = new Stack<char>(Console.ReadLine());
+            while (charact.Count > 0)
+            {
+                char dt = charact.Pop();
+                Console.Write(dt);
+            }
+        }
+    }
+}
