91634b3636a84352406e4df4e76cfa635cb1b0de
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-10 23:41:45 +0200

    Fix second exercise and upload third

---
+++ Programming Fundamentals/Stacks and Queues/Stacks and Queues/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NSX = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(nums);
+
+            int s = NSX[1];
+            for (int i = 0; i < s; i++)
+            {
+                if (queue.Count == 0)
+                {
+                    Console.WriteLine("0");
+                    break;
+                }
+                queue.Dequeue();
+            }
+
+            int x = NSX[2];
+            if (queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else if (!queue.Contains(x) && queue.Count != 0)
+            {
+                Console.WriteLine(queue.Min());
+            }
+
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
