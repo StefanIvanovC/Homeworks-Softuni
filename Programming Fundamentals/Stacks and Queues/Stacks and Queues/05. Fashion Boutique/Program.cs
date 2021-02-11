a31c857ba97fefdd6af8cc330cf43b8b39c95c7e
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-11 23:33:16 +0200

    add homewor

---
+++ Programming Fundamentals/Stacks and Queues/Stacks and Queues/05. Fashion Boutique/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clothValues = Console.ReadLine().Split(' ').Select(int.Parse).Reverse().ToList();
+            var capacity = int.Parse(Console.ReadLine());
+
+            var racks = 0;
+            var sum = 0;
+
+            var box = new Stack<int>(clothValues);
+
+            while (box.Count > 0)
+            {
+                var currentSize = sum + box.Peek();
+
+                if (currentSize < capacity)
+                {
+                    sum += box.Pop();
+                }
+                else if (currentSize == capacity)
+                {
+                    racks++;
+                    box.Pop();
+                    sum = 0;
+                }
+                else
+                {
+                    racks++;
+                    sum = box.Pop();
+                }
+            }
+
+            if (sum > 0)
+            {
+                racks++;
+            }
+
+            Console.WriteLine(racks);
+        }
+    }
+}
