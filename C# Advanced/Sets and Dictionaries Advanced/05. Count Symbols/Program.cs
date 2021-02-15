c9f0701feed045f4ab54bb68c547a8a7be428617
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-15 23:56:56 +0200

    Add homework

---
+++ C# Advanced/Sets and Dictionaries Advanced/05. Count Symbols/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> ocurrences = new SortedDictionary<char, int>();
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var charCurrent = text[i];
+                if (ocurrences.Keys.Contains(charCurrent))
+                {
+                    ocurrences[charCurrent]++;
+                    continue;
+                }
+
+                if (!ocurrences.Keys.Contains(charCurrent))
+                {
+                    ocurrences.Add(charCurrent, 0);
+                    ocurrences[charCurrent]++;
+                }
+            }
+
+            foreach (var character in ocurrences)
+            {
+                Console.WriteLine($"{character.Key}: {character.Value} time/s");
+            }
+        }
+    }
+}
