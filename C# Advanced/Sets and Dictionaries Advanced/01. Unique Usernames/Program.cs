a844729414f1db2e36d56987a24bbd3ff5abd20b
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-15 23:51:20 +0200

    Add homework

---
+++ C# Advanced/Sets and Dictionaries Advanced/01. Unique Usernames/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.UniqueNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string names = Console.ReadLine();
+                set.Add(names);
+
+            }
+
+            foreach (string name in set)
+            {
+                Console.WriteLine(string.Join(" ", name));
+            }
+        }
+    }
+}
