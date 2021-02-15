88fa0656be8e616c452b0ba81d18e2adf598815b
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-15 23:55:11 +0200

    Add homework

---
+++ C# Advanced/Sets and Dictionaries Advanced/03. Periodic Table/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> names = new SortedSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                var uniqueName = Console.ReadLine().Split();
+                for (var j = 0; j < uniqueName.Length; j++)
+                {
+                    var element = uniqueName[j];
+                    names.Add(element);
+                }
+            }
+            foreach (var name in names)
+            {
+                Console.Write(String.Join(" ", name) + " ");
+            }
+        }
+    }
+}
---
+++ C# Advanced/Sets and Dictionaries Advanced/04. Even Times/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            Dictionary<int, int> nums = new Dictionary<int, int>();
+            for (int i = 0; i < input; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!nums.Keys.Contains(num))
+                {
+                    nums.Add(num, 0);
+                }
+
+                nums[num]++;
+
+            }
+
+            foreach (var item in nums)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                    break;
+                }
+            }
+        }
+    }
+}
