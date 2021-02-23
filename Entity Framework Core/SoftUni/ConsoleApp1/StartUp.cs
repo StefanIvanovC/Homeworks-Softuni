277ecefbe375d0997e014c8b4a584431bbb2bf9f
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-23 22:49:06 +0200

    Add Entity Framework Select and LINQ expresions tasks

---
+++ Entity Framework Core/SoftUni/ConsoleApp1/StartUp.cs
@@ -0,0 +1,66 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var softUniContex = new SoftUniContext();
+            var result = GetEmployeesWithSalaryOver50000(softUniContex);
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(x => new {
+                    x.EmployeeId,
+                    x.FirstName,
+                    x.MiddleName,
+                    x.LastName,
+                    x.JobTitle,
+                    x.Salary
+                })
+                .OrderBy(x => x.EmployeeId)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+            }
+
+            var result = sb.ToString().TrimEnd();
+            return result;
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.Salary
+                })
+                .Where(x => x.Salary > 50000)
+                .OrderBy(x => x.FirstName)
+                .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+
+            var result = sb.ToString().TrimEnd();
+            return result;
+        }
+
+    }
+}
