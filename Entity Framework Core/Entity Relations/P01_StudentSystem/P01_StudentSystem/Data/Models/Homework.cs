339142d636450bd43f5265b7868d148a294188bd
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-02-26 20:01:24 +0200

    Add mapping table for student class

--- Entity Framework Core/Entity Relations/P01_StudentSystem/P01_StudentSystem/Data/Models/Homework.cs
+++ Entity Framework Core/Entity Relations/P01_StudentSystem/P01_StudentSystem/Data/Models/Homework.cs
@@ -15,6 +15,16 @@ namespace P01_StudentSystem.Data.Models
         public string Content { get; set; }
 
         public ContentType ContentType { get; set; }
+
+        public DateTime SubmissionTime { get; set; }
+
+        public int StudentId { get; set; }
+
+        public Student Student { get; set; }
+
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
     }
 }
 //  o	HomeworkId
