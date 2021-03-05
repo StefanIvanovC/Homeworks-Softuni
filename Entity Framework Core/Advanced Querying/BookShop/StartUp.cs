4ffbb1713f388d43cfd9c9fe3a44b8e365db7018
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-03-05 20:37:41 +0200

    Add new homework For Advanced Quering with parse of enum entity and print by string.Join(EnviromentNewLine)

---
+++ Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetBooksByAgeRestriction(db, "miNor"));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var ageRestrictionParse = Enum.Parse<AgeRestriction>(command, true);
+            var books = context.Books
+                .Where(books =>books.AgeRestriction == ageRestrictionParse)
+                .Select(books => books.Title)
+                .OrderBy(title => title)
+                .ToArray();
+
+
+            var result = "";
+
+            foreach (var bookTitle in books)
+            {
+                result = string.Join(Environment.NewLine, books);
+            }
+
+            return result;
+
+        }
+    }
+}
