31226c231fd5d60b5bbfb915f305e53d229a7c98
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-03-05 21:36:54 +0200

    Add third exercise

--- Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
+++ Entity Framework Core/Advanced Querying/BookShop/StartUp.cs
@@ -5,6 +5,7 @@
     using Initializer;
     using System;
     using System.Linq;
+    using System.Text;
 
     public class StartUp
     {
@@ -13,7 +14,7 @@
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
 
-            Console.WriteLine(GetGoldenBooks(db));
+            Console.WriteLine(GetBooksByPrice(db));
         }
 
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
@@ -57,5 +58,39 @@
             return result;
 
         }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var booksWithPrice = context.Books
+                .Where(x => x.Price > 40)
+                .Select(x => new
+                {
+                    Title = x.Title,
+                    Price = x.Price
+                })
+                .OrderByDescending(x => x.Price)
+                .ToArray();
+
+
+            var sb = new StringBuilder();
+
+            foreach (var book in booksWithPrice)
+            {
+                sb.AppendLine($"{book.Title} - ${book.Price:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+
+        }
+
+
+        //public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        //{
+        //    //var enumParse = Enum.Parse<relea>
+
+        //    //var books = context.Books
+        //    //    .Where(x => x.ReleaseDate != year)
+        //    //    .Select 
+        //}
     }
 }
