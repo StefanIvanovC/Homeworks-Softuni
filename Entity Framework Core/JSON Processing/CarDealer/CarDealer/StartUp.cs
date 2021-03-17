35c6989a78fb0064544dd309707ca82728a2ae11
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-03-17 14:27:30 +0200

    Add homework

---
+++ Entity Framework Core/JSON Processing/CarDealer/CarDealer/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTO;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        static IMapper mapper;
+
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            var json = File.ReadAllText("../../../Datasets/suppliers.json");
+
+            var result = ImportSuppliers(context, json);
+
+            Console.WriteLine(result);
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson) 
+        {
+            var suppliersDto = JsonConvert
+                .DeserializeObject<IEnumerable<ImportSupplierInputModel>>(inputJson);
+
+            var suppliers = suppliersDto.Select(x => new Supplier
+            {
+                Name = x.Name,
+                IsImporter = x.isImporter
+            })
+            .ToList();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}.";
+        }
+
+        //private static void InitializeAutoMapper()
+        //{
+        //    var config = new MapperConfiguration(cfg =>
+        //    {
+        //        cfg.AddProfile <ImportSupplierInputModel>();
+        //});
+        //    mapper = config.CreateMapper();
+        //}
+    }
+}
